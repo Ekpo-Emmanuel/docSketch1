b7ccf5465a8dc9f485589bbce7040df66a3f8a3a
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-06-28 23:09:59 -0700

    Refactor  component: - Add  and file query logic - Include  component for content display - Fix  prop for responsive design - Update  component to accept  prop - Refactor for responsive design with fixed min sizes - Add background color and border to  panel

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,53 +1,69 @@
-'use client'
+"use client";
 
-import React, {useState, useEffect} from 'react'
-import { Editor } from '../_components/Editor'
+import React, { useState, useEffect } from "react";
+import { Editor } from "../_components/Editor";
 import WorkSpaceHeader from "@/app/(routes)/workspace/_components/WorkSpaceHeader";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { useMediaQuery } from 'react-responsive';
+} from "@/components/ui/resizable";
+import { useMediaQuery } from "react-responsive";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { File } from "../../dashboard/_components/_fileDisplay/File1";
+import Canvas from "../_components/Canvas";
 
-export default function Workspace({...props}: any) {
+export default function Workspace({ params }: any) {
   const [triggerSave, setTriggerSave] = useState(false);
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' }); 
-  const direction = isSmallScreen ? 'vertical' : props.direction || 'horizontal'; 
+  const [fileData, setFileData] = useState<File | any>();
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
+  const direction = isSmallScreen ? 'vertical' : params.direction || 'horizontal';
   const contentMinSize = isSmallScreen ? 20 : 50;
   const canvasMinSize = isSmallScreen ? 20 : 50;
+  const convex = useConvex();
 
+  useEffect(() => {
+    console.log("FILEID", params.fileId);
+    params.fileId && getFileData();
+  }, []);
+
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, {
+      _id: params.fileId,
+    });
+    setFileData(result);
+  };
 
   return (
-    <div className='px-2'>
-      <WorkSpaceHeader onSave={() => setTriggerSave(!triggerSave)}/>
-
-      {/* <div className='grid grid-cols-1 lg:grid-cols-2 mt-3 m-2 rounded-sm'>
-        <div className=' h-screen rounded-sm p-2 border b-1'>
-          <Editor onSaveTrigger={triggerSave} fileId={params.fileId}/>
-        </div>
-
-        <div className=' h-screen rounded-sm p-2 bg-blue-500'>
-          Canvas
-        </div>
-      </div> */}
-
-    <ResizablePanelGroup
-      direction={direction} // Use dynamic direction
-      className="rounded-lg border min-h-[600px]  rounded-lg border"
-    >
-      <ResizablePanel defaultSize={50} minSize={30}>
-        <div className=' h-screen rounded-sm p-2 border b-1'>
-          <Editor onSaveTrigger={triggerSave} fileId={props.params.fileId}/>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={canvasMinSize} minSize={canvasMinSize}>
-        <div className="flex h-full">
-          <span className="font-semibold">Content</span>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    <div className="h-screen">
+      <WorkSpaceHeader onSave = {() => setTriggerSave(!triggerSave)} />
+      <div className="bg-white">
+        <ResizablePanelGroup
+          direction = {direction}
+          className="rounded-lg border min-h-[600px]"
+        >
+          <ResizablePanel defaultSize = {50} minSize = {30}>
+            <div className="h-full rounded-sm p-2 border b-1">
+              <Editor
+                onSaveTrigger = {triggerSave}
+                fileId = {params.fileId}
+                fileData = {fileData}
+              />
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize = {canvasMinSize} minSize = {canvasMinSize}>
+            <div className="h-screen border-l">
+              <Canvas
+                onSaveTrigger = {triggerSave}
+                fileId = {params.fileId}
+                fileData = {fileData}
+              />
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
-  )
+  );
 }
