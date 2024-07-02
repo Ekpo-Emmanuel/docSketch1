e71c9fd66886c145c327833e8f380de37e20199f
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-02 16:42:21 -0700

    feat: added toggle between document and canvas

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -17,6 +17,9 @@ import Canvas from "../_components/Canvas";
 export default function Workspace({ params }: any) {
   const [triggerSave, setTriggerSave] = useState(false);
   const [fileData, setFileData] = useState<File | any>();
+  const [bothVisible, setBothVisible] = useState(true);
+  const [onlyDocumentVisible, setOnlyDocumentVisible] = useState(false);
+  const [onlyCanvasVisible, setOnlyCanvasVisible] = useState(false);
   const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
   const direction = isSmallScreen ? 'vertical' : params.direction || 'horizontal';
   const contentMinSize = isSmallScreen ? 20 : 50;
@@ -39,6 +42,26 @@ export default function Workspace({ params }: any) {
     }
   };
 
+  const handleBothVisible = () => {
+    setBothVisible(true);
+    setOnlyDocumentVisible(true);
+    setOnlyCanvasVisible(true);
+  };
+
+  const handleOnlyDocumentVisible = () => {
+    setBothVisible(false);
+    setOnlyDocumentVisible(true);
+    setOnlyCanvasVisible(false);
+
+    console.log(onlyCanvasVisible, onlyDocumentVisible, bothVisible);
+  };
+
+  const handleOnlyCanvasVisible = () => {
+    setBothVisible(false);
+    setOnlyDocumentVisible(false);
+    setOnlyCanvasVisible(true);
+  }
+
   return (
     <div className="h-screen">
       <div className="p-3 grid gap-2">
@@ -48,6 +71,27 @@ export default function Workspace({ params }: any) {
             fileName = {fileData?.name} 
           />
         </div>
+        <div className="inline-flex mx-auto w-fit overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
+          <button 
+            className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300"
+            onClick={handleBothVisible}
+          >
+            Both
+          </button>
+          <button 
+            className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+            onClick={handleOnlyDocumentVisible}
+          >
+            Document
+          </button>
+          <button 
+            className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+            onClick={handleOnlyCanvasVisible}
+          >
+            Canvas
+          </button>
+        </div>
+        {bothVisible && 
         <div className="bg-white">
           <ResizablePanelGroup
             direction = {direction}
@@ -73,7 +117,23 @@ export default function Workspace({ params }: any) {
               </div>
             </ResizablePanel>
           </ResizablePanelGroup>
-        </div>
+        </div>}
+        {onlyDocumentVisible && 
+          <div className="h-screen rounded-sm border-1 border-t-200 oveflow-hidden max-h-[700px]">
+            <Editor
+              onSaveTrigger = {triggerSave}
+              fileId = {params.fileId}
+              fileData = {fileData}
+            />
+          </div>}
+        {onlyCanvasVisible && 
+          <div className="h-full rounded-sm border-l ">
+            <Canvas
+              onSaveTrigger = {triggerSave}
+              fileId = {params.fileId}
+              fileData = {fileData}
+            />
+          </div>}
       </div>
     </div>
   );
