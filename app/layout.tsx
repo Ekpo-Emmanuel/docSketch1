7127ac502521c1b543ce7b0ee7bd8b06974f40c0
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-03 06:53:44 -0700

    refactor: clean up RootLayout component by removing unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,16 @@
 import type { Metadata } from "next";
-import { Inter, Roboto, Rubik } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
 import { Toaster } from "@/components/ui/sonner"
-// import localFont from '@next/font/local';
-
 const inter = Inter({ 
   subsets: ["latin"] 
 });
-
 
 export const metadata: Metadata = {
   title: "DocSketch",
   description: "Documents & diagrams for students and teams",
 };
-
-// const myFont = localFont({ src: '../public/fonts/nexa/Nexa-Heavy.ttf' })
-
-
 
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
