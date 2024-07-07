317508e50b97b19960e024d540f4f0f21970f921
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-06 19:08:23 -0700

    style: Announcement Component

---
+++ app/_components/Announcement.tsx
@@ -0,0 +1,7 @@
+export default function Announcement() {
+    return (
+        <div className="px-3 py-1 text-[12px] text-white text-center bg-blue-500">
+            We have migrated from Editor.js to Lexical
+        </div>
+    )
+}
