8cc636ab56b1fd7ac6c5db443a284fa4db358307
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-06 19:05:08 -0700

    feat: add Announcement component to Home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 // import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 import { useEffect, useState } from "react";
@@ -8,23 +8,24 @@ import Header from "../_components/Header";
 import Hero from "../_components/Hero";
 import LaunchingSoon from "../_components/LaunchingSoon";
 import LoadingAnimation from "../_components/LoadingAnimation";
+import Announcement from "../_components/Announcement"
 
 export default function Home() {
   // const {user} = useKindeBrowserClient();
 
-  
   // useEffect(() => {
   //   if (user) {
   //     // console.log(user)
   //     console.log('User is logged in')
   //   }
   // }, [user])
-  
+
   //loading animation
- 
+
   return (
     <>
       {/* <LoadingAnimation show={loading} /> */}
+      <Announcement />
       <Header />
       <Hero />
       <Features />
