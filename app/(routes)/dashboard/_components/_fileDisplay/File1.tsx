2b91bdcd22a280f9940288afd40a33dad4ee3104
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-06 06:54:30 -0700

    refactor(File1): Implement non-archived file filtering and improve file management

--- app/(routes)/dashboard/_components/_fileDisplay/File1.tsx
+++ app/(routes)/dashboard/_components/_fileDisplay/File1.tsx
@@ -27,90 +27,85 @@ export interface File {
 }
 
 export default function File1() {
-  //functions: search, create project, export table, getFiles, pagnition, Edits (Rename, Delete, Share, Add to Team),
-  const { fileList_, setFileList_ } = useContext(FileListContext);
+  //functions: (search), (create project), export table, (getFiles), pagnition, Edits (Rename, Delete, Share, Add to Team, archieve),
+  const { allFiles, nonArchivedFiles, setAllFiles, isSubscribed } = useContext(FileListContext);
   const [fileList, setFileList] = useState<any>();
+  const [displayedFiles, setDisplayedFiles] = useState<any[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const { user }: any = useKindeBrowserClient();
   const router = useRouter();
   const deleteFilesMutation = useMutation(api.files.deleteFilesById);
-  const renameFilesMutation = useMutation(api.files.renameFIle);
+  const renameFilesMutation = useMutation(api.files.renameFile);
+  const achiveFileMutation = useMutation(api.files.archiveFile);
 
   useEffect(() => {
-    fileList_ && setFileList(fileList_);
-  }, [fileList_]);
+    if (nonArchivedFiles) {
+      setFileList(nonArchivedFiles);
+      setDisplayedFiles(nonArchivedFiles);
+    }
+  }, [nonArchivedFiles]);
 
+  console.log("FileList", fileList);
   //Create Project
   const onProjectCreate = (name: string) => {
     console.log("New File Created", name);
   }
-
-  /**
-   * Updates the search query and filters the file list based on the query.
-   *
-   * @param {string} query - The search query.
-   * @return {void}
-   */
+  
   const searchForProject = (query: string) => {
     setSearchQuery(query);
     if (query) {
-      const filteredFiles = fileList_.filter((file: File) =>
+      const filteredFiles = nonArchivedFiles.filter((file: File) =>
         file?.name?.toLowerCase().includes(query.toLowerCase())
       );
-      setFileList(filteredFiles);
+      setDisplayedFiles(filteredFiles);
     } else {
-      setFileList(fileList_);
+      setDisplayedFiles(nonArchivedFiles);
     }
-  }
-  
-  /**
-   * Deletes a project by its ID.
-   *
-   * @param {any} fileId - The ID of the file to be deleted.
-   * @return {Promise<void>} A promise that resolves when the file is successfully deleted.
-   */
+  };
+
   const deleteProject = async (fileId: any) => {
     try {
       await deleteFilesMutation({ fileId: fileId });
-      const updatedFileList = fileList.filter((file: any) => file._id !== fileId);
-      setFileList(updatedFileList);
-      setFileList_(updatedFileList);
-
+      const updatedAllFiles = allFiles.filter((file: any) => file._id !== fileId);
+      setAllFiles(updatedAllFiles);
       toast.success('File Deleted Successfully');
     } catch (error) {
       console.error('Error deleting project:', error);
     }
-  }
+  };
 
-  /**
-   * Renames a project by its ID.
-   *
-   * @param {any} fileId - The ID of the file to be renamed.
-   * @param {string} fileName - The new name for the file.
-   * @return {Promise<void>} A promise that resolves when the file is successfully renamed.
-   */
+
   const renameProject = async (fileId: any, fileName: string) => {
     try {
       await renameFilesMutation({ _id: fileId, name: fileName });
-    
-      const updatedFileList = fileList.map((file: any) => 
+      const updatedAllFiles = allFiles.map((file: any) => 
         file._id === fileId ? { ...file, name: fileName } : file
       );
-
-      setFileList(updatedFileList);
-      setFileList_(updatedFileList);
-
+      setAllFiles(updatedAllFiles);
       toast.success('File Renamed Successfully');
     } catch (error) {
-      console.log('Error renaming project', error)
+      console.log('Error renaming project', error);
     }
-  }
+  };
+
+  const archiveProject = async (fileID: any) => {
+    try {
+      await achiveFileMutation({ _id: fileID, archieve: true });
+      const updatedAllFiles = allFiles.map((file: any) => 
+        file._id === fileID ? { ...file, archive: true } : file
+      );
+      setAllFiles(updatedAllFiles);
+      toast.success('File Archived Successfully');
+    } catch (error) {
+      console.log('Error archiving project', error);
+    }
+  };
 
   return (
     <section className="container px-4 mx-auto">
       <TableTitle 
         onProjectCreate={onProjectCreate} 
-        fileList={fileList_.length} 
+        fileList={displayedFiles.length} 
         onSearch={searchForProject}
       />
         <div className="flex flex-col mt-6">
@@ -120,13 +115,14 @@ export default function File1() {
                 className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg"
                 style = {{ overflowY: "auto", maxHeight: "550px" }}
               > 
-                {fileList && fileList.length > 0 ? (
+                {displayedFiles && displayedFiles.length > 0 ?  (
                   <table className="relative min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                     <TableHeader />
                     <TableBody 
-                      fileList = {fileList}
+                      fileList={displayedFiles}
                       deleteProject = {deleteProject}
                       renameProject = {renameProject}
+                      archiveProject = {archiveProject}
                     />
                   </table>
                 ) : searchQuery ? <NotFound /> : <EmptyTable />}
