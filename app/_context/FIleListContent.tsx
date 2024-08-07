314f4a50159dba137aeab5906f8b0bb1646883cf
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-07 06:54:33 -0700

    fix: update FileListContext with nonTrashedFiles and improve type safety

--- app/_context/FIleListContent.tsx
+++ app/_context/FIleListContent.tsx
@@ -4,6 +4,7 @@ import { createContext } from "react";
 interface FileListContextType {
     allFiles: any[];
     nonArchivedFiles: any[];
+    nonTrashedFiles: any[];
     setAllFiles: React.Dispatch<React.SetStateAction<any[]>>;
     isSubscribed: boolean;
 }
