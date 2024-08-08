a905ebc7814791501fd1d919293686520648322f
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-08 05:38:31 -0700

    style: Adjusted padding between elements

--- app/(routes)/dashboard/_components/_fileDisplay/TablePagination.tsx
+++ app/(routes)/dashboard/_components/_fileDisplay/TablePagination.tsx
@@ -9,18 +9,18 @@ interface TablePaginationProps {
 
 export default function TablePagination( props: TablePaginationProps) {
   return (
-    <div className="hidden mt-6 sm:flex sm:items-center sm:justify-between">
-      <div className="text-sm text-gray-500 dark:text-gray-400">
+    <div className="mt-6 flex items-center justify-between">
+      <div className="text-xs sm:text-sm text-gray-500 dark:text-gray-400">
         Page{" "}
         <span className="font-medium text-gray-700 dark:text-gray-100">
           {props.currentPage} of {props.totalPages}
         </span>
       </div>
-      <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
+      <div className="flex items-center gap-x-4">
         <button
           onClick={props.onPrevious}
           disabled={props.currentPage === 1}
-          className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="flex items-center justify-center w-1/2 px-3 sm:px-5 py-2 text-xs sm:text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ export default function TablePagination( props: TablePaginationProps) {
         <button
           onClick={props.onNext}
           disabled={props.currentPage === props.totalPages}
-          className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="flex items-center justify-center w-1/2 px-3 sm:px-5 py-2 text-xs sm:text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           <span>Next</span>
           <svg
